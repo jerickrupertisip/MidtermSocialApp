53d534d9d824836703de6a92522d0373c1c023f5
NexushasTaken <nexushastaken@gmail.com>
2026-03-06 01:55:06 +0800

    add image authors

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import "dart:convert";
 import "dart:ui";
+import "package:http/http.dart" as http;
 import "package:shadcn_flutter/shadcn_flutter.dart";
+import "package:uniso_social_media_app/models/picsum_image.dart";
 
 void main() {
   runApp(const MyApp());
@@ -39,95 +42,143 @@ class Scroller extends ScrollBehavior {
 }
 
 class _Home extends State<Home> {
-  var pageController = PageController(initialPage: 0);
+  final _pageController = PageController(initialPage: 0);
+  final List<PicsumImage> _images = [];
+
+  int _currentPage = 0;
+  int _currentPicsumPage = 0;
+  bool _isLoading = false;
+
+  Future<List<PicsumImage>> fetchImages(int page, {int? limit = 4}) async {
+    final response = await http.get(
+      Uri.parse("https://picsum.photos/v2/list?page=$page&limit=$limit"),
+    );
+
+    if (response.statusCode == 200) {
+      List<dynamic> data = jsonDecode(response.body);
+      return data.map((item) => PicsumImage.fromJson(item)).toList();
+    } else {
+      throw Exception("Failed to load images");
+    }
+  }
+
+  Future<void> _fetchNextPage() async {
+    if (_isLoading) return;
+
+    setState(() => _isLoading = true);
+
+    try {
+      var newImages = await fetchImages(_currentPicsumPage);
+
+      setState(() {
+        _images.addAll(newImages);
+        _currentPicsumPage++;
+        _isLoading = false;
+      });
+    } catch (e) {
+      setState(() => _isLoading = false);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _fetchNextPage();
+    _pageController.addListener(() {
+      if (_currentPage > _images.length - 2) {
+        _fetchNextPage();
+      }
+    });
+  }
 
   @override
   void dispose() {
-    pageController.dispose();
+    _pageController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      child: ScrollConfiguration(
-        behavior: Scroller(),
-        child: Stack(
-          alignment: .center,
-          children: [
-            PageView.builder(
-              controller: pageController,
-              scrollDirection: Axis.vertical,
-              itemBuilder: (context, index) {
-                var size = MediaQuery.of(context).size;
+    return ScrollConfiguration(
+      behavior: Scroller(),
+      child: Stack(
+        alignment: .center,
+        children: [
+          PageView.builder(
+            controller: _pageController,
+            scrollDirection: Axis.vertical,
+            itemCount: _images.length,
+            onPageChanged: (page) {
+              setState(() {
+                _currentPage = page;
+              });
+            },
+            itemBuilder: (context, index) {
+              var image = _images[index];
+              // var size = MediaQuery.of(context).size;
 
-                return Stack(
-                  children: [
-                    Positioned.fill(
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(
-                          Theme.of(context).radiusMd,
-                        ),
-                        child: Image.network(
-                          "https://picsum.photos/seed/${index * 40}/${(size.width * 2).toInt()}/${(size.height * 2).toInt()}",
-                          fit: BoxFit.cover,
-                          frameBuilder:
-                              (context, child, frame, wasSynchronouslyLoaded) {
-                                if (frame != null) return child;
-
-                                return Container(
-                                  color: Colors.black,
-                                  alignment: Alignment.center,
-                                  child: CircularProgressIndicator(
-                                    size: Theme.of(
-                                      context,
-                                    ).typography.x8Large.fontSize,
-                                  ),
-                                );
-                              },
-                        ),
-                      ),
-                    ),
-
-                    Positioned(
-                      child: Container(
-                        child: Text(
-                          "Hello, World",
-                          style: TextStyle(
-                            shadows: [
-                              Shadow(
-                                offset: Offset.fromDirection(10, 2),
-                                blurRadius: 6,
+              return ClipRRect(
+                borderRadius: BorderRadius.circular(Theme.of(context).radiusMd),
+                child: Image.network(
+                  // "https://picsum.photos/seed/${index * 40}/${(size.width * 2).toInt()}/${(size.height * 2).toInt()}",
+                  image.downloadUrl,
+                  fit: BoxFit.cover,
+                  frameBuilder:
+                      (context, child, frame, wasSynchronouslyLoaded) {
+                        if (frame != null) {
+                          return Stack(
+                            children: [
+                              Positioned.fill(child: child),
+                              Positioned(
+                                bottom: 0.0,
+                                left: 0.0,
+                                child: Container(
+                                  child: Text(
+                                    image.author,
+                                    style: TextStyle(
+                                      shadows: [
+                                        Shadow(
+                                          offset: Offset.fromDirection(10, 2),
+                                          blurRadius: 6,
+                                        ),
+                                      ],
+                                    ),
+                                  ).h1,
+                                ).withPadding(all: Theme.of(context).radiusXl),
                               ),
                             ],
-                          ),
-                        ).h1,
-                      ).withPadding(all: Theme.of(context).radiusXl),
-                    ),
-                  ],
-                );
-              },
-            ),
+                          );
+                        }
 
-            Positioned(
-              bottom: 0.0,
-              left: 0.0,
-              child: Container(
-                child: Text(
-                  "Hello, World",
-                  style: TextStyle(
-                    shadows: [
-                      Shadow(
-                        offset: Offset.fromDirection(10, 2),
-                        blurRadius: 6,
-                      ),
-                    ],
-                  ),
-                ).h1,
-              ).withPadding(all: Theme.of(context).radiusXl),
-            ),
-          ],
-        ),
+                        return Container(
+                          color: Colors.black,
+                          alignment: Alignment.center,
+                          child: CircularProgressIndicator(
+                            size: Theme.of(context).typography.x8Large.fontSize,
+                          ),
+                        );
+                      },
+                ),
+              );
+            },
+          ),
+
+          Positioned(
+            top: 0.0,
+            left: 0.0,
+            child: Container(
+              child: Text(
+                "Hello, World",
+                style: TextStyle(
+                  shadows: [
+                    Shadow(offset: Offset.fromDirection(10, 2), blurRadius: 6),
+                  ],
+                ),
+              ).h1,
+            ).withPadding(all: Theme.of(context).radiusXl),
+          ),
+        ],
       ),
     ).withPadding(all: Theme.of(context).typography.small.fontSize);
   }
